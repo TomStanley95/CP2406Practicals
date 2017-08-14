e9b48535b8a51a4dc99243aa9850e3fa0ddaf402
TomStanley95 <tom.stanley@my.jcu.edu.au>
2017-08-14 20:15:36 +1000

    Completed 2 questions from Task 5

---
+++ Prac01/Task05/FavouriteSong.java
@@ -0,0 +1,7 @@
+public class FavouriteSong {
+    public static void main(String[] args) {
+        String lyrics = "We're no strangers to love \nYou know the rules and so do I\n" +
+                "A full commitment's what I'm thinking of\nYou wouldn't get this from any other guy";
+        System.out.print(lyrics);
+    }
+}
---
+++ Prac01/Task05/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("It's over, Anakin! I have the high ground!");
+        System.out.println("Obi-Wan Kenobi");
+        System.out.println("Star Wars Episode 3: Revenge of the Sith");
+        System.out.println("2005");
+
+    }
+}
